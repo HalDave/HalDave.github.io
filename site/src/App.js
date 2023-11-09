14c2cecd70283a965b97847633a966dc09685f2d
david.garcia <david.garcia@sunwebgroup.com>
2023-11-09 11:13:34 +0100

    Update App.js

--- site/src/App.js
+++ site/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Created as poart of Sunweb's TechLab #11
+          Created as part of Sunweb's TechLab #11
         </p>
       </header>
     </div>
