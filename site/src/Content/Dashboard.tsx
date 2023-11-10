114698c1a5d2376aebd39f8c864a0a24aa4f581b
david.garcia <david.garcia@sunwebgroup.com>
2023-11-10 22:39:18 +0100

    Hobbies

--- site/src/Content/Dashboard.tsx
+++ site/src/Content/Dashboard.tsx
@@ -1,36 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Grid } from '@mui/material';
-import GridItem from '../UI/GridItem';
-
-const URI = 'https://haldave-github-pages-webapp.azurewebsites.net/Items/Books'
+import React from 'react';
 
 
 const Dashboard = () => {
-  const [items, setitems] = useState([])
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch(URI)
-        if (!response.ok) return
-        const results = await response.json()
-        setitems(results)
-      } catch (error) {
-        console.log(error)
-      }
-    })()
-  }, [])
   return (
     <div>
       <h1>Dashboard</h1>
-      <Grid container columnSpacing={{ xs: 1, sm: 2, md: 6, lg: 12 }} direction="row"
-        alignItems="center"
-        justifyContent="center"
-        sx={{ paddingRight: 6, paddingLeft: 6 }}>
-        {items && items.map((item: any) => (
-          <GridItem key={item.id} item={item}/>
-        ))}
-      </Grid>
     </div>
   );
 }
--- site/src/Content/Hobbies.tsx
+++ site/src/Content/Hobbies.tsx
@@ -1,9 +1,58 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Grid } from '@mui/material';
+import GridItem from '../UI/GridItem';
 
+const URI = (type:string)=>{ return `https://haldave-github-pages-webapp.azurewebsites.net/Items/${type}`}
 const Hobbies = () => {
+  const [books, setBooks] = useState([])
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(URI('Books'))
+        if (!response.ok) return
+        const results = await response.json()
+        setBooks(results)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [])
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(URI('Movies'))
+        if (!response.ok) return
+        const results = await response.json()
+        setMovies(results)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [])
+
   return (
     <div>
       <h1>Hobbies</h1>
+      <h2>Books</h2>
+      <Grid container columnSpacing={{ xs: 1, sm: 2, md: 6, lg: 12 }} direction="row"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ paddingRight: 6, paddingLeft: 6 }}>
+        {books && books.map((item: any) => (
+          <GridItem key={item.id} item={item}/>
+        ))}
+      </Grid>
+      <h2>Movies</h2>
+      <Grid container columnSpacing={{ xs: 1, sm: 2, md: 6, lg: 12 }} direction="row"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ paddingRight: 6, paddingLeft: 6 }}>
+        {movies && movies.map((item: any) => (
+          <GridItem key={item.id} item={item}/>
+        ))}
+      </Grid>
     </div>
   );
 }
