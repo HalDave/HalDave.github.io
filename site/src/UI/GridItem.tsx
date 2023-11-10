ccffe5e2a3f37a9366b23f42a1e78836668215da
david.garcia <david.garcia@sunwebgroup.com>
2023-11-10 15:49:46 +0100

    GridItem

--- site/src/UI/GridItem.tsx
+++ site/src/UI/GridItem.tsx
@@ -7,6 +7,7 @@ interface ItemProps {
 }
 
 interface ItemDataProps {
+  title: string
   rating: number;
   opinion: string;
   image: string;
@@ -16,8 +17,8 @@ const GridItem = ({ item }: { item: ItemProps }) => {
   return (
     <Grid item xs={9} sm={6} md={3} lg={2} sx={{ maxWidth: "480px", minWidth: "480px" }}>
       <Card>
-        <CardHeader title={item.id} />
-        <CardMedia component="img" height="250" image={item.data.image} alt="green iguana" />
+        <CardHeader title={item.data.title} />
+        <CardMedia component="img" height="250" image={item.data.image} alt={item.data.title} sx={{objectFit: "contain"}} />
         <CardContent>
           <Typography variant="body1">{item.data.opinion}</Typography>
         </CardContent>
